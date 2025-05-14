11073337b6d02cf3d957d1bf1d57e962773fb1d8
soda-daichi <soda.daichi@aainc.co.jp>
2025-05-14 18:19:52 +0900

    マイグレーション作成完了

---
+++ database/migrations/2025_05_14_083506_create_diary_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiaryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('diary', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->String('title',128);
+            $table->text('contents');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('diary');
+    }
+}
