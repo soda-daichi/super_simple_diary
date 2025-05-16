2a9b206da2765b92e0e3c40926f7a9f363325488
soda-daichi <soda.daichi@aainc.co.jp>
2025-05-16 10:40:29 +0900

    コントローラーの名前を変更しました！

--- app/Http/Controllers/showdiariesController.php
+++ app/Http/Controllers/showdiariesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class showdiariesController extends Controller
+class showDiariesController extends Controller
 {
     public function show(Request $request) {
          $diaries = DB::table('diaries')->get();
